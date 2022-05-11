9ba6ca783341a72f429f11d7c712ea7c3cac063a
gongkai624929 <gongkai624929@163.com>
2022-05-11 16:15:16 +0800

    Initial commit

---
+++ app/src/main/java/com/volit/utils/ToolUtils.java
@@ -0,0 +1,8 @@
+package com.volit.utils;
+
+public class ToolUtils {
+    public static final boolean isEmpty(String str){
+        if(str==null || str.length()==0) return true;
+        return false;
+    }
+}
